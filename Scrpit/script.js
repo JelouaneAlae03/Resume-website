59dfc83b5f5ddce2d6cddca82c7166645754c1dc
jelouane alae <jel.alae37@gmail.com>
2023-01-04 00:23:05 +0100

    ++

---
+++ Scrpit/script.js
@@ -0,0 +1,10 @@
+$(Document).ready(function(){
+    // $(".Card-service-link").hover(function(){
+    //     $(".Card-service-link-icon").css({"margin-left": "10rem"});
+    // });
+
+
+    // $(".Card-service-link").after(function(){
+    //     $(".Card-service-link-icon").css({"margin-left": ""});
+    // });
+});
