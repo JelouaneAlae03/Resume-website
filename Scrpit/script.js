ca81be9712a020c8a82678b7e18fc8a7c4134a37
jelouane alae <jel.alae37@gmail.com>
2023-01-19 15:48:29 +0100

    left bar scroll

--- Scrpit/script.js
+++ Scrpit/script.js
@@ -7,6 +7,38 @@ $(Document).ready(function(){
     // $(".Card-service-link").after(function(){
     //     $(".Card-service-link-icon").css({"margin-left": ""});
     // });
+
+    // $(".personal-info").on('scroll',function(){
+    //     $("#line-first").hide();
+    // });
+
+    // $("#card-personal-info").animate({
+    //     scrollTop: 
+    //     $("#line-first","#line-second").hide()
+    // }); 
+
+
+
+    var lastScrollTop =0;
+    $(".personal-info").scroll(function(event){
+        var st = $(this).scrollTop();
+        if (st < lastScrollTop){
+            $("#line-first").css('opacity',1);
+            $("#line-second").css('opacity',1);
+            } else {
+            $("#line-first").css('opacity',1);
+            $("#line-second").css('opacity',1);
+        }
+        lastScrollTop = st;
+
+
+    });
+
+
+
+
+
+
 });
 
 
