2d5ea5ad8353adc7eed41ad4673f7e194b39d861
jelouanealae <jel.alae37@gmail.com>
2023-02-09 09:08:52 +0100

    Verical Navbar

--- Scrpit/script.js
+++ Scrpit/script.js
@@ -17,17 +17,66 @@ $(Document).ready(function(){
     //     $("#line-first","#line-second").hide()
     // }); 
 
-    $(".right-part").hover(function(){
-        $(".navbar-container").toggle();
-        $(".right-part").css("width","15rem");
-        $("#navbar-button-icon").removeClass("bi bi-list").addClass("bi bi-x-lg");
-    });
+    // $(".right-part").hover(function(){
+    //     $(".navbar-container").toggle();
+    //     $(".right-part").css("width","14rem");
+    //     $("#navbar-button-icon").removeClass("bi bi-list").addClass("bi bi-x-lg");
+    //     $(".menu-tittle").hide();
+    //     $(".header").css("filter","blur(1px)");
+    //     $(".number-info").css("filter","blur(1px)");
+    //     $(".Services").css("filter","blur(1px)");
+    //     $(".Education").css("filter","blur(1px)");
+    //     $(".complete-project-part").css("filter","blur(1px)");
+    //     $(".recommendation-container").css("filter","blur(1px)");
+    //     $(".contact-us-part").css("filter","blur(1px)");
+    //     $(".get-in-touch-container").css("filter","blur(1px)");
+    // });
 
 
-    $(".right-part").mouseleave(function(){
-        $(".right-part").css("width","");
-        $("#navbar-button-icon").removeClass("bi bi-x-lg").addClass("bi bi-list");
-    });
+    
+
+
+    // $(".right-part").mouseleave(function(){
+    //     $(".right-part").css("width","");
+    //     $("#navbar-button-icon").removeClass("bi bi-x-lg").addClass("bi bi-list");
+        
+    //     $(".menu-tittle").show();
+
+
+
+    //     $(".header").css("filter","blur(0)");
+    //     $(".number-info").css("filter","blur()");
+    //     $(".Services").css("filter","blur()");
+    //     $(".Education").css("filter","blur()");
+    //     $(".complete-project-part").css("filter","blur()");
+    //     $(".recommendation-container").css("filter","blur()");
+    //     $(".contact-us-part").css("filter","blur()");
+    //     $(".get-in-touch-container").css("filter","blur()");
+
+
+
+        
+    // });
+
+    // $(".navbar-link").click(function(){
+    //     $(".right-part").css("width","");
+    //     $("#navbar-button-icon").removeClass("bi bi-x-lg").addClass("bi bi-list");
+        
+    //     $(".menu-tittle").show();
+
+
+
+    //     $(".header").css("filter","blur(0)");
+    //     $(".number-info").css("filter","blur()");
+    //     $(".Services").css("filter","blur()");
+    //     $(".Education").css("filter","blur()");
+    //     $(".complete-project-part").css("filter","blur()");
+    //     $(".recommendation-container").css("filter","blur()");
+    //     $(".contact-us-part").css("filter","blur()");
+    //     $(".get-in-touch-container").css("filter","blur()");
+
+
+    // })
 
 
     var lastScrollTop =0;
@@ -81,16 +130,4 @@ $(Document).ready(function(){
         $("#get-in-touch-icon3").css({"background-color" : "#033540"});
         $("#get-in-touch-icon3").css({"color" : "white"});
     });
-    
-
-
-
-
-
-
-
-
-
-
-
 });
