d71a2fbfc27cd60952a43734637b34aa9a0e9887
JELOUANE Alae <jel.alae37@gmail.com>
2023-07-02 11:24:06 +0100

    Order Service + typed Text Header

--- Scrpit/script.js
+++ Scrpit/script.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
     });
 
     $(".left-bar").hide();
+    $("#ServicesModal").hide();
+
     $(".left-menu-icon").click(function () {
       $(".left-bar").toggle();
       // $(".main-content-page").css({'filter':'blur(1px)'});
@@ -21,12 +23,12 @@ $(document).ready(function () {
     });
 
     $("body").mouseup(function (e) {
-        var container = $(".left-bar");
-        // if the target of the click isn't the container nor a descendant of the container
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-          container.hide();
-        }
-      });
+      var container = $(".left-bar");
+      // if the target of the click isn't the container nor a descendant of the container
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+      }
+    });
   } else {
     //desktop mode
   }
@@ -148,18 +150,14 @@ $(document).ready(function () {
     var txtEmail = document.getElementById("txtEmail").value;
     var txtMessage = document.getElementById("txtMessage").value;
 
-    var content =
-      "Name " +
-      txtName +
-      "----" +
-      "Email " +
-      txtEmail +
-      "----" +
-      "Message " +
-      txtMessage;
+    var content ='<p><strong><span style="font-family: Helvetica;">Name&nbsp;</span></strong><span style="font-family: Helvetica;"><strong>:&nbsp;</strong>'+txtName+'</span></p><p><span style="font-family: Helvetica;"><strong>Email</strong> <strong>:&nbsp;</strong>'+txtEmail+'</span></p><p><span style="font-family: Helvetica;"><strong>Message : </strong>'+txtMessage+'</span></p>'
+
+    // var content ='	<div class="container">		<div class="space-y-4 mb-6">			<h1 class="text-4xl fw-800">New Client</h1>		</div>		<div class="card rounded-3xl px-4 py-8 p-lg-10 mb-6">			<table class="p-2 w-full">				<tbody>					<tr>						<td>Name</td>						<td class="text-right">'+txtName+'</td>					</tr>					<tr>						<td>Email</td>						<td class="text-right">'+txtName+'</td>					</tr>					<tr>						<td>Message</td>						<td class="text-right">'+txtMessage+'</td>					</tr>				</tbody>			</table>			<hr class="my-6">		</div>	</div>';
+
+
 
     Email.send({
-      SecureToken: "e25da4b0-622e-4a93-b301-b32ca5bb6072", //add your token here
+      SecureToken: "e25da4b0-622e-4a93-b301-b32ca5bb6072",
       To: "jel.alae37@gmail.com",
       From: "jel.alae37@gmail.com",
       Subject: "New client From Portfolio",
@@ -188,17 +186,62 @@ $(document).ready(function () {
   //     let NewDiv = document.createElement('div');
   //     NewDiv.className = 'imagePanel';
   //     $('body').append(NewDiv);
-
   // }
 
   $(".card-img").on("click", function () {
     let ImgPickeraID = $(this).attr("id");
     let img = document.getElementById(ImgPickeraID);
     let NewImage = img.cloneNode(true);
-    $("#imagepreview").attr("src", $(NewImage).attr("src")); // here asign the image to the modal when the user click the enlarge link
-    $("#imagemodal").modal("show"); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+    $("#imagepreview").attr("src", $(NewImage).attr("src"));
+    $("#imagemodal").modal("show");
   });
+  
 
   //close the left bar when the user click outside of it
-  
+
+  $("#OrderNowServices").on("click", function () {
+    $("#ServicesModal").modal("show");
+  });
+
+  $("#btnCloseModal").on("click", function () {
+    $("#ServicesModal").modal("hide");
+  });
+
+
+  $("#OrderNowServices1 , #OrderNowServices2 , #OrderNowServices3").on(
+    "click",
+    function () {
+      $("#txtMessage").val("");
+      var ParentId = $("#" + $(this).attr("id"))
+        .parent()
+        .attr("id");
+      var FisrtChildText = $("#" + ParentId + ">h2").text();
+      if (FisrtChildText) {
+        var Message;
+        if (FisrtChildText == "Front-End Development") {
+          Message =
+            "I wanted to get in touch with you about an exciting collaboration opportunity in front-end development.";
+        } else if (FisrtChildText == "Back-End Development") {
+          Message =
+            "I wanted to get in touch with you about an exciting collaboration opportunity in Back-End Development.";
+        } else if (FisrtChildText == "Sound Design") {
+          Message =
+            "I wanted to get in touch with you about an exciting collaboration opportunity in Sound Design.";
+        }
+         $("#txtMessage").val(Message);
+      }
+
+      // alert('sallam');
+    }
+  );
+  var typed = new Typed(".multiple-text",{
+    strings: ["WorkSpace!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true});
+
+
+
+    
 });
